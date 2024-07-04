e20c054a3732d8cf4d02ae96bcd152e4ad417a08
Aryal <100072339+aryalbishnu@users.noreply.github.com>
2024-07-04 23:35:30 +0900

    first commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("hi");
+    System.out.println("hello");
+    System.out.println("hello connect to jenkins");
+
+  }
+}
